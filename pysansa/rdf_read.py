93737fff185fae81071a54400aea90c5aeb9af3e
Pahulmeet Singh <pahulmeetsingh@gmail.com>
2021-02-27 21:14:53 +0100

    Update rdf_read.py

--- pysansa/rdf_read.py
+++ pysansa/rdf_read.py
@@ -1,39 +1,24 @@
 import findspark
 findspark.init()
-
 import pyspark as ps
 from pyspark import SparkContext, SparkConf
 
-#conf = SparkConf().setAppName("SANSA TESTING")
-#sc = SparkContext(conf = conf)
-
-
-
-#ret_rdf = sansa_rdf_io_pack.RDFReader(spark_session_scala).rdf(file)
-
 def read_rdf_file(file,jar_home):
-    
     conf = SparkConf().set("spark.jars", jar_home)
     sc = SparkContext( conf=conf)
-
-    print("Entered RDF Reader_____________________________________________")
-
-    sansa_rdf_io = sc._jvm.net.sansa_stack.rdf.spark.io
-    sansa_rdf_io_pack = sc._jvm.net.sansa_stack.rdf.spark.io.package
-
+    sansa_rdf_io = sc._jvm.net.sansa_stack.rdf.spark.io.package  #The package to be used  
     spark_session_scala = sc._jvm.org.apache.spark.sql.SparkSession.builder().master("local").config("spark.serializer", "org.apache.spark.serializer.KryoSerializer").config("spark.sql.legacy.allowUntypedScalaUDF", "true").appName("SansaTEST").getOrCreate()
-
-    lang_lib = sc._jvm.org.apache.jena.riot.Lang
-    lang = lang_lib.NTRIPLES
-    
-    ret_rdf = sansa_rdf_io_pack.RDFReader(spark_session_scala).rdf(file)
-    
+   
+    ret_rdf = sansa_rdf_io.RDFReader(spark_session_scala).rdf(file)  
     return ret_rdf
-    #print(ret_rdf)
-    #print(dir(ret_rdf))
-    
-    
-    
+        
+def print_triples(ret,size):
+    arr = ret.take(size)
+    idx = 0
+    for i in arr:
+        idx = idx+1
+        print(idx,"->",i.toString())
 
-def add10(num):
-    print(num+10)
+def print_count(ret):
+    print("Count = ", ret.count())        
+    
